4cc4f350d0c808a6e8ac1856f9b97c1ddceafa53
ByteAtATime <byteatatime@proton.me>
2026-07-07 22:20:01 +0800

    feat: add extra fields to package

--- src/package.rs
+++ src/package.rs
@@ -3,6 +3,9 @@ pub struct Package {
     pub repo: Option<String>,
     pub description: Option<String>,
     pub version: String,
+    pub maintainer: Option<String>,
+    pub provides: Vec<String>,
+    pub conflicts: Vec<String>,
 }
 
 impl From<&alpm::Package> for Package {
@@ -12,6 +15,9 @@ impl From<&alpm::Package> for Package {
             repo: pkg.db().map(|x| x.name().to_string()),
             description: pkg.desc().map(|x| x.to_string()),
             version: pkg.version().to_string(),
+            maintainer: pkg.packager().map(|x| x.to_string()),
+            provides: pkg.provides().iter().map(|x| x.to_string()).collect(),
+            conflicts: pkg.conflicts().iter().map(|x| x.to_string()).collect(),
         }
     }
 }
