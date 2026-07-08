4c3e0ae4c98c3dd2c857dd0b96088f16e914b719
ByteAtATime <byteatatime@proton.me>
2026-07-08 10:07:15 +0800

    refactor: extract PakajoIcon to new file

---
+++ src/icon.rs
@@ -0,0 +1,49 @@
+use anyhow::{Result, anyhow};
+use gpui::{App, AssetSource, IntoElement, RenderOnce, SharedString, Window};
+use gpui_component::{Icon, IconNamed};
+use rust_embed::RustEmbed;
+
+#[derive(IntoElement)]
+pub enum PakajoIcon {
+    History,
+    Scale,
+}
+
+impl IconNamed for PakajoIcon {
+    fn path(self) -> SharedString {
+        match self {
+            PakajoIcon::History => "icons/history.svg",
+            PakajoIcon::Scale => "icons/scale.svg",
+        }
+        .into()
+    }
+}
+
+impl RenderOnce for PakajoIcon {
+    fn render(self, _: &mut Window, _: &mut App) -> impl IntoElement {
+        Icon::new(self)
+    }
+}
+
+#[derive(RustEmbed)]
+#[folder = "./assets"]
+#[include = "icons/**/*.svg"]
+pub struct Assets;
+
+impl AssetSource for Assets {
+    fn load(&self, path: &str) -> Result<Option<std::borrow::Cow<'static, [u8]>>> {
+        if path.is_empty() {
+            return Ok(None);
+        }
+
+        Self::get(path)
+            .map(|f| Some(f.data))
+            .ok_or_else(|| anyhow!("could not find asset at path \"{path}\""))
+    }
+
+    fn list(&self, path: &str) -> Result<Vec<SharedString>> {
+        Ok(Self::iter()
+            .filter_map(|p| p.starts_with(path).then(|| p.into()))
+            .collect())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,11 @@
+mod icon;
 mod package;
 
 use alpm::{Alpm, SigLevel};
-use anyhow::anyhow;
 use gpui::*;
 use gpui_component::*;
-use rust_embed::RustEmbed;
 
-use crate::package::Package;
-
-#[derive(IntoElement)]
-enum PakajoIcon {
-    History,
-    Scale,
-}
-
-impl IconNamed for PakajoIcon {
-    fn path(self) -> SharedString {
-        match self {
-            PakajoIcon::History => "icons/history.svg",
-            PakajoIcon::Scale => "icons/scale.svg",
-        }
-        .into()
-    }
-}
-
-impl RenderOnce for PakajoIcon {
-    fn render(self, _: &mut Window, _: &mut App) -> impl IntoElement {
-        Icon::new(self)
-    }
-}
-
-#[derive(RustEmbed)]
-#[folder = "./assets"]
-#[include = "icons/**/*.svg"]
-pub struct Assets;
-
-impl AssetSource for Assets {
-    fn load(&self, path: &str) -> Result<Option<std::borrow::Cow<'static, [u8]>>> {
-        if path.is_empty() {
-            return Ok(None);
-        }
-
-        Self::get(path)
-            .map(|f| Some(f.data))
-            .ok_or_else(|| anyhow!("could not find asset at path \"{path}\""))
-    }
-
-    fn list(&self, path: &str) -> Result<Vec<SharedString>> {
-        Ok(Self::iter()
-            .filter_map(|p| p.starts_with(path).then(|| p.into()))
-            .collect())
-    }
-}
+use crate::{icon::PakajoIcon, package::Package};
 
 struct PackageListing {
     pkg: Package,
@@ -179,7 +133,7 @@ fn main() {
             .unwrap();
     }
 
-    let app = gpui_platform::application().with_assets(Assets);
+    let app = gpui_platform::application().with_assets(icon::Assets);
 
     app.run(move |cx| {
         gpui_component::init(cx);
