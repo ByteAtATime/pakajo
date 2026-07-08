348df3472e0d29bb1b2768316eb7e59c8f3858d1
ByteAtATime <byteatatime@proton.me>
2026-07-08 09:51:53 +0800

    feat: add custom icons

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,101 @@
 mod package;
 
 use alpm::{Alpm, SigLevel};
+use anyhow::anyhow;
 use gpui::*;
 use gpui_component::*;
+use rust_embed::RustEmbed;
 
 use crate::package::Package;
 
+#[derive(IntoElement)]
+enum PakajoIcon {
+    History,
+}
+
+impl IconNamed for PakajoIcon {
+    fn path(self) -> SharedString {
+        match self {
+            PakajoIcon::History => "icons/history.svg",
+        }
+        .into()
+    }
+}
+
+impl RenderOnce for PakajoIcon {
+    fn render(self, _: &mut Window, _: &mut App) -> impl IntoElement {
+        Icon::new(self)
+    }
+}
+
+#[derive(RustEmbed)]
+#[folder = "./assets"]
+#[include = "icons/**/*.svg"]
+pub struct Assets;
+
+impl AssetSource for Assets {
+    fn load(&self, path: &str) -> Result<Option<std::borrow::Cow<'static, [u8]>>> {
+        if path.is_empty() {
+            return Ok(None);
+        }
+
+        Self::get(path)
+            .map(|f| Some(f.data))
+            .ok_or_else(|| anyhow!("could not find asset at path \"{path}\""))
+    }
+
+    fn list(&self, path: &str) -> Result<Vec<SharedString>> {
+        Ok(Self::iter()
+            .filter_map(|p| p.starts_with(path).then(|| p.into()))
+            .collect())
+    }
+}
+
 struct PackageListing {
     pkg: Package,
+}
+
+impl PackageListing {
+    fn format_name(&self) -> String {
+        if let Some(repo) = self.pkg.repo.clone() {
+            format!("{}/{}", repo, self.pkg.name)
+        } else {
+            self.pkg.name.clone()
+        }
+    }
+
+    fn info_bar(&self) -> impl IntoElement {
+        div().h_flex().gap_4().child(
+            div()
+                .h_flex()
+                .gap_2()
+                .child(PakajoIcon::History)
+                .child(self.pkg.version.clone()),
+        )
+    }
+
+    fn header(&self) -> impl IntoElement {
+        div()
+            .v_flex()
+            .child(div().text_2xl().child(self.format_name()))
+            .child(self.info_bar())
+            .children(
+                self.pkg
+                    .description
+                    .clone()
+                    .map(|desc| div().text_color(rgb(0x444444)).child(desc)),
+            )
+    }
 }
 
 impl Render for PackageListing {
     fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
         div()
             .v_flex()
-            .bg(rgb(0xcccccc))
             .gap_2()
             .p_4()
             .size_full()
-            .child(format!(
-                "{}/{}",
-                self.pkg.repo.clone().unwrap_or_default(),
-                self.pkg.name.clone()
-            ))
-            .child(self.pkg.description.clone().unwrap_or_default())
-            .child(self.pkg.version.clone())
+            .child(self.header())
     }
 }
 
@@ -36,7 +107,7 @@ impl Render for PakajoRoot {
     fn render(&mut self, _: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         let mut package: Option<&alpm::Package> = None;
         for database in self.alpm_handle.syncdbs() {
-            if let Ok(pkg) = database.pkg("ripgrep") {
+            if let Ok(pkg) = database.pkg("code") {
                 package = Some(pkg);
                 break;
             }
@@ -104,7 +175,7 @@ fn main() {
             .unwrap();
     }
 
-    let app = gpui_platform::application().with_assets(gpui_component_assets::Assets);
+    let app = gpui_platform::application().with_assets(Assets);
 
     app.run(move |cx| {
         gpui_component::init(cx);
