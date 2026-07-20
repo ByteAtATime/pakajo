6f33ff2bf9049a4d5ed22c38fb85813dbc61bc77
ByteAtATime <byteatatime@proton.me>
2026-07-20 19:16:35 +0800

    feat: add installed indicator for opt dependencies

--- src/package_detail.rs
+++ src/package_detail.rs
@@ -1,8 +1,11 @@
 use crate::{
-    icon::PakajoIcon, install::InstallProgress, package::Package, root::PakajoRoot,
+    icon::PakajoIcon,
+    install::InstallProgress,
+    package::{Package, dep_base_name},
+    root::PakajoRoot,
     utils::format_bytes,
 };
-use gpui::*;
+use gpui::{prelude::FluentBuilder, *};
 use gpui_component::{
     button::{Button, ButtonVariants as _},
     spinner::Spinner,
@@ -349,6 +352,7 @@ impl PackageDetail {
                     .v_flex()
                     .gap_2()
                     .children(self.pkg.opt_dependencies.iter().map(|dep| {
+                        let is_installed = self.installed_names.contains(dep_base_name(&dep.name));
                         div()
                             .h_flex()
                             .items_center()
@@ -356,8 +360,20 @@ impl PackageDetail {
                             .w_full()
                             .px_4()
                             .py_3()
-                            .bg(cx.theme().secondary)
-                            .child(div().font_bold().child(dep.name.clone()))
+                            .bg(if is_installed {
+                                // TODO: urgh color mixing again
+                                cx.theme().green.opacity(0.1)
+                            } else {
+                                cx.theme().secondary
+                            })
+                            .child(
+                                div()
+                                    .font_bold()
+                                    .when(is_installed, |s| {
+                                        s.text_color(cx.theme().green.saturation(0.6))
+                                    })
+                                    .child(dep.name.clone()),
+                            )
                             .children(dep.reason.clone().map(|r| div().child(r)))
                     })),
             )
